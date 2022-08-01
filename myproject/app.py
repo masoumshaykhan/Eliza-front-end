9675cce7b0d8c630e1586f4395f913f195431dfe
masoum shaykhan <masumsheykhan@gmail.com>
2022-08-01 16:15:17 +0430

    send data from html file to flask file.

--- myproject/app.py
+++ myproject/app.py
@@ -1,27 +1,17 @@
 from eliza import *
-from flask import Flask,render_template,url_for,redirect,request
+from flask import Flask, render_template, request
 app = Flask(__name__)
-app.secret_key="masum"
-
 @app.route('/')
-def home():
-    return render_template('home.html')
-  
-@app.route('/message', methods=['POST' , 'GET'])  
-def send_message():
-    if request.method == 'POST':
-        message=request.form["msg"]
-        print(1111)
-        print(message)
-        # message = the message we should pass to eliza respond method
-        return redirect(url_for("message",msg=message))
-    else:
-        return render_template('home.html')
+def student():
+   return render_template('home.html')
 
-@app.route("/<msg>")
-def show_message(msg):
-    # msg= message we should give from eliza and pass to html
-    return "<h1>{msg}</h1>".format(msg=msg)
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug = True)
